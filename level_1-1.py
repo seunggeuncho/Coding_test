3d4ab38341e82041f49d904ee8d9c57398d6107a
Cho <wkfyddl1tp@naver.com>
2022-02-23 23:43:15 +0900

    level1

---
+++ level_1-1.py
@@ -0,0 +1,11 @@
+def solution(id_list, report, k):
+    answer = [0 for i in range(len(id_list))]
+    num = [0 for i in range (len(id_list))]
+    for i in report:
+        num[id_list.index(i.split(' ')[1])] += 1 
+    for i in range (len(num)):
+        if num[i] >= k:
+            for j in report:
+                if id_list[i] == j.split(' ')[1]:
+                    answer[id_list.index(j.split(' ')[0])] += 1
+    return answer
