5d884d168408e3da8567711940007581f61b76c9
Cho <wkfyddl1tp@naver.com>
2022-02-27 22:46:49 +0900

    level2

--- level_2/오픈채팅방.py
+++ level_2/오픈채팅방.py
@@ -1,12 +1,39 @@
-def solution(record):
-    
+def enter(ex):
+    ex = ex + "님이 들어왔습니다."
+    return ex
+
+def leave(ex):
+    ex = ex + "님이 나갔습니다."
+    return ex
+def list(record):
+    list = [0 for i in range(len(record))]
     for i in range(len(record)):
-        if record[i].split(' ')[0] == "Change":
+        if record[i].split(' ')[0] == "Leave":
+            for j in range(i):
+                if record[j].split(' ')[1] == record[i].split(' ')[1] and record[j].split(' ')[0] != "Leave" :
+                    list[i] = record[j].split(' ')[2]
+        elif record[i].split(' ')[0] == "Change":
+            list[i] = 0
+        else:
+            list[i] = record[i].split(' ')[2]
+    return list
+    
+def solution(record):
+    lst = list(record)
+    print(lst)
+        
+    for i in range(len(record)):
+        if record[i].split(' ')[0] == "Change" or record[i].split(' ')[0] == "Enter":
             for j in range(i):
                 if record[j].split(' ')[1] == record[i].split()[1]:
-                    record[j].replace(record[j].split(' ')[2],record[i].split()[2])
-                    print(record[j].split()[2])
-    print(record)
-    answer=[]
-    
+                    lst[j] = record[i].split()[2]
+                    
+    for i in range(len(record)):
+        if record[i].split(' ')[0] == "Enter":
+            lst[i] = enter(lst[i])
+        elif record[i].split(' ')[0] == "Leave":
+            lst[i] = leave(lst[i])
+        elif lst[i] == 0:
+            del lst[i]
+    answer = lst
     return answer
