658c7b94c6d6be12e306917e3fefabe5a80d375d
Cho <wkfyddl1tp@naver.com>
2022-03-06 22:38:20 +0900

    level3

--- level_3/추석트래픽.py
+++ level_3/추석트래픽.py
@@ -3,8 +3,9 @@ def solution(lines):
     for line in lines:
         times = line.split()
         time, sec = times[1], times[2]
-        
-    
-        
+        hour = time.split(':')[0]
+        min = time.split(':')[1]
+        second = time.split(':')[2]
+        print(hour, min, second)
     
     return answer
