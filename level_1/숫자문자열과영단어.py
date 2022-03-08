9cd05a0d69ce4730d28e2a53345836e6e231d2c5
Cho <wkfyddl1tp@naver.com>
2022-03-08 22:59:10 +0900

    level1

--- level_1/숫자문자열과영단어.py
+++ level_1/숫자문자열과영단어.py
@@ -1,3 +1,6 @@
 def solution(s):
     answer = 0
+    num_dict = [{'one':1} , {'two' : 2}, {'three' : 3}, {'four' : 4}, {'five' : 5} ,{'six' : 6}, {'seven' : 7}, {'eight' : 8}, {'nine' : 9}]
+    print(num_dict)
+
     return answer
