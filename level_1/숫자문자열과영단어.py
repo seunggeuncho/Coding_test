da2ce352d08bce209619727b86b7f6ca7a61a0ba
Cho <wkfyddl1tp@naver.com>
2022-03-10 00:19:20 +0900

    final

--- level_1/숫자문자열과영단어.py
+++ level_1/숫자문자열과영단어.py
@@ -1,5 +1,5 @@
 def solution(s):
-    num_dict = {'one':1,'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5 ,'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
+    num_dict = {'zero': 0, 'one':1,'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5 ,'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
     s = list(s)
     alpha=[]
     answer =[]
@@ -14,7 +14,5 @@ def solution(s):
                 str_alpha = ''.join(alpha)
                 answer.append(num_dict[str_alpha])
                 alpha.clear()
-    answer = list(map(str, answer))
-    
-    answer=''.join(answer)
+    answer = ''.join(list(map(str, answer)))    
     return int(answer)
