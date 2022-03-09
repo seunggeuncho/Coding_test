b975290a0445fa612b62e8f90b2c75eec2917789
Cho <wkfyddl1tp@naver.com>
2022-03-09 23:30:47 +0900

    level1

--- level_1/숫자문자열과영단어.py
+++ level_1/숫자문자열과영단어.py
@@ -1,6 +1,20 @@
 def solution(s):
-    answer = 0
-    num_dict = [{'one':1} , {'two' : 2}, {'three' : 3}, {'four' : 4}, {'five' : 5} ,{'six' : 6}, {'seven' : 7}, {'eight' : 8}, {'nine' : 9}]
-    print(num_dict)
-
-    return answer
+    num_dict = {'one':1,'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5 ,'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
+    s = list(s)
+    alpha=[]
+    answer =[]
+    wer="one"
+    for i in range(len(s)):
+        if s[i].isdigit():
+            answer.append(s[i])
+            continue
+        else:
+            alpha.append(s[i])
+            if(''.join(alpha) in num_dict):
+                str_alpha = ''.join(alpha)
+                answer.append(num_dict[str_alpha])
+                alpha.clear()
+    answer = list(map(str, answer))
+    
+    answer=''.join(answer)
+    return int(answer)
