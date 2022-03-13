751327de8f1c94cebe6fea72a87511b14ce042d5
unknown <wkfyddl1tp@naver.com>
2022-03-13 21:06:56 +0900

    modify

--- level_1/키패드누르기.py
+++ level_1/키패드누르기.py
@@ -19,13 +19,25 @@ def solution(numbers, hand):
         elif num in center:
             if num == 0:
                 num = 11
-            if now_left + 2 == now_right:
+            tm_left = now_left
+            tm_right = now_right
+            if now_left in center:
+                if now_left > num:
+                    tm_left -= 2
+                elif now_left < num:
+                    tm_left += 2
+            if now_right in center:
+                if now_right > num:
+                    tm_right -= 2
+                elif now_right < num:
+                    tm_right += 2
+            if tm_left + 2 == tm_right or tm_left == tm_right:
                 answer.append(hand)
                 if hand == "R":
                     now_right = num
                 else:
                     now_left = num
-            elif abs(now_left - num) > abs(now_right - num):
+            elif abs(tm_left - num) > abs(tm_right - num):
                 answer.append("R")
                 now_right = num
             else:
