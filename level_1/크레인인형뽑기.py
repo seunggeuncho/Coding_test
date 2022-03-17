2583e931f9f93d04b2d3a6442e68002664e5ee89
unknown <wkfyddl1tp@naver.com>
2022-03-17 13:49:27 +0900

    modify

--- level_1/크레인인형뽑기.py
+++ level_1/크레인인형뽑기.py
@@ -5,10 +5,11 @@ def solution(board, moves):
     bucket = []
     for select in moves:
         height = 0
-        while (board[height][select - 1] == 0):
+        while (board[height][select - 1] == 0 and height < len(board[0]) - 1):
             height += 1
-            print(height)
-        bucket.append(board[height][select - 1])
-        board[height][select - 1] = 0
+        if (board[height][select - 1] != 0):
+            bucket.append(board[height][select - 1])
+            board[height][select - 1] = 0
+        print(bucket)
     return answer
 solution(board, moves)
