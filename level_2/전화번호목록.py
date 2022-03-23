f9ab689e72bcba13e57847239f999f032d156fb2
unknown <wkfyddl1tp@naver.com>
2022-03-23 17:40:04 +0900

    final

--- level_2/전화번호목록.py
+++ level_2/전화번호목록.py
@@ -1,9 +1,11 @@
 def solution(phone_book):
     answer = True
     index = 1
-    for standard in phone_book:
-        for compare_num in range(index, len(phone_book)):
-            if phone_book[compare_num].rfind(standard) == 0:
-                return False
-        index += 1
+    phone_book.sort(key=len)
+    phone_book.sort()
+    for index in range(len(phone_book)):
+        if index + 1 == len(phone_book):
+            break
+        if phone_book[index + 1].rfind(phone_book[index]) == 0:
+            return False
     return answer
