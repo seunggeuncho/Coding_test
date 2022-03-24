88a5191f0a2d1f1a06e2421da0d8eec021043faf
unknown <wkfyddl1tp@naver.com>
2022-03-24 23:27:38 +0900

    final

--- level_2/기능개발.py
+++ level_2/기능개발.py
@@ -1,3 +1,19 @@
+import math
 def solution(progresses, speeds):
     answer = []
+    during = []
+    for idx in range(len(progresses)):
+        during.append(math.ceil((100 - progresses[idx]) / speeds[idx]))
+    idx = 0
+    while (idx < len(during)):
+        pluse = 1
+        if idx + pluse == len(during):
+            answer.append(1)
+            break
+        while (during[idx] >= during[idx + pluse]):
+            pluse += 1
+            if idx + pluse == len(during):
+                break
+        answer.append(pluse)
+        idx += pluse
     return answer
