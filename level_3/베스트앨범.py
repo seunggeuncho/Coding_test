b3e3cff49d95646088cd2f41d71b00fc7354e383
unknown <wkfyddl1tp@naver.com>
2022-03-27 21:15:27 +0900

    modify

--- level_3/베스트앨범.py
+++ level_3/베스트앨범.py
@@ -1,3 +1,10 @@
 def solution(genres, plays):
+    total = {}
+    for idx in range(len(genres)):
+        if genres[idx] in total.keys():
+            total[genres[idx]] += plays[idx]
+        else:
+            total[genres[idx]] = plays[idx]
+    print(total)
     answer = []
     return answer
