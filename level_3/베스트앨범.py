c6bb1612c06faeb2dd189a9c360106c94a11bb35
unknown <wkfyddl1tp@naver.com>
2022-03-30 02:28:10 +0900

    modify

--- level_3/베스트앨범.py
+++ level_3/베스트앨범.py
@@ -1,11 +1,25 @@
-def solution(genres, plays):    
+def solution(genres, plays):
     total = {}
+    answer = []
     for idx in range(len(genres)):
         if genres[idx] in total.keys():
             total[genres[idx]] += plays[idx]
         else:
             total[genres[idx]] = plays[idx]
-    print(total.sort())
-    find = max(total, key = total.get)
-    answer = []
+    while total: 
+        play_li = []
+        max_album = max(total, key = total.get)
+        for idx in range(len(genres)):
+            if max_album == genres[idx]:
+                play_li.append(plays[idx])
+        play_li.sort(reverse = True)
+        if len(play_li) > 2:
+            play_li = play_li[:2]
+        while play_li:
+            find = play_li.pop(0)
+            for idx in range(len(genres)):
+                if genres[idx] == max_album and plays[idx] == find:
+                    answer.append(idx)
+        del total[max_album]
+    
     return answer
