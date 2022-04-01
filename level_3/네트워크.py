c81474fb409ae4c924df9d6f51a049be4d51a0da
unknown <wkfyddl1tp@naver.com>
2022-04-01 23:09:20 +0900

    modify

--- level_3/네트워크.py
+++ level_3/네트워크.py
@@ -1,10 +1,3 @@
 from collections import defaultdict
 def solution(n, computers):
-    edges = defaultdict(list)
-    for idx in range(len(computers)):
-        for idx2 in range(len(computers[idx])):
-            if idx != idx2 and computers[idx][idx2] == 1:
-                edges[idx].append(idx2)
-    print(edges)
-    answer = 0
-    return answer
+    
