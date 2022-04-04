69e2a5708a328b96194dd73a263577f7f091e528
unknown <wkfyddl1tp@naver.com>
2022-04-05 00:05:39 +0900

    modify

--- level_3/네트워크.py
+++ level_3/네트워크.py
@@ -1,10 +1,19 @@
-def solution(n, computers):    
+def solution(n, computers):
     answer = 0
     visit = [0 for i in range(n)]
+    def DFS(idx):
+        if visit[idx] == 1:
+            print(idx)
+            nonlocal answer
+            answer += 1
+            return 
+        else:
+            visit[idx] = 1
+            for num in range(n):
+                if computers[idx][num] == 1 and idx != num:
+                    DFS(num)
+                    
     for idx in range(n):
         if visit[idx] == 0:
-            visit[idx] = 1
             DFS(idx)
-    def DFS(idx):
-        if computers[idx]
     return answer
