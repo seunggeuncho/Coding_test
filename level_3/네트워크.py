aa7cfed49a84b110ff91dfb4dba9ba02340c4648
unknown <wkfyddl1tp@naver.com>
2022-04-04 23:50:22 +0900

    modify

--- level_3/네트워크.py
+++ level_3/네트워크.py
@@ -1,3 +1,10 @@
-from collections import defaultdict
-def solution(n, computers):
-    
+def solution(n, computers):    
+    answer = 0
+    visit = [0 for i in range(n)]
+    for idx in range(n):
+        if visit[idx] == 0:
+            visit[idx] = 1
+            DFS(idx)
+    def DFS(idx):
+        if computers[idx]
+    return answer
