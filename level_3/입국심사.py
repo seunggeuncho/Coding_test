cda753d370e299e20483ee2d93e7571de20ba3c4
unknown <wkfyddl1tp@naver.com>
2022-04-12 23:48:37 +0900

    final

--- level_3/입국심사.py
+++ level_3/입국심사.py
@@ -1,3 +1,17 @@
 def solution(n, times):
     answer = 0
+    left = 1
+    right = max(times) * n
+    while left < right:
+        mid = (left + right) // 2
+        total = 0
+        for time in times:
+            total += mid // time
+        print(total)
+        if total >= n:
+            right = mid
+        else:
+            left = mid + 1
+        
+    answer = left
     return answer
