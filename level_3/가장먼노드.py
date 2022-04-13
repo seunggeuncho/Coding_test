53c441e89979f016a3c5a20e4d1475a6119bc683
unknown <wkfyddl1tp@naver.com>
2022-04-14 03:05:48 +0900

    modify

--- level_3/가장먼노드.py
+++ level_3/가장먼노드.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import queue
+import heapq
 def solution(n, edge):
     node_dict = defaultdict(list)
     for node in edge:
@@ -7,9 +7,18 @@ def solution(n, edge):
             node_dict[node[1]].append(node[0])
     print(node_dict)
     dis = {node:float('inf') for node in range(1, n + 1)}
-    print(dis)
     dis[1] = 0
     queue = []
     heapq.heappush(queue,[dis[1] , 1])
+    while queue:
+        current_dis,current_node = heapq.heappop(queue)
+        if dis[current_node] < current_dis:
+            continue
+        
+        for adjacent in node_dict[current_node]:
+            distance = current_dis + 1
+            if distance < dis[adjacent]:
+                dis[adjacent] = distance
+                heapq.heappush(queue,[adjacent])
     answer = 0
     return answer
