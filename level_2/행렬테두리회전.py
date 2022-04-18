081c7ea8d5d41ae8f1c8e45c1623db1612974a6d
unknown <wkfyddl1tp@naver.com>
2022-04-19 02:59:11 +0900

    modify

--- level_2/행렬테두리회전.py
+++ level_2/행렬테두리회전.py
@@ -1,3 +1,12 @@
 def solution(rows, columns, queries):
-    
+    lst = [[i + (columns * j) for i in range(1, rows + 1)] for j in range(0, columns)]
+    for query in queries:
+        temp = []
+        left_row , left_col , right_row, right_col = query[0],query[1],query[2],query[3]
+        for idx in range(left_row, right_row):
+            for idx2 in range(left_col, right_col):
+                if (idx == left_row or idx == right_row) and (idx2 == left_col or idx2 == right_col):
+                    temp.append(query[idx][idx2])
+    print(temp)
+    answer = 0
     return answer
