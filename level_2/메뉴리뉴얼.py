143ea7a2ca85b61e20b20c6c8106899a8b8d0fb6
unknown <wkfyddl1tp@naver.com>
2022-04-24 23:50:05 +0900

    modify

--- level_2/메뉴리뉴얼.py
+++ level_2/메뉴리뉴얼.py
@@ -1,6 +1,35 @@
+from collections import defaultdict
+orders=["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course=[2,3,4]
 def solution(orders, course):
+    def DFS(lst,order, num):
+        nonlocal check
+        if len(lst) == num:
+            nonlocal ords
+            lst.sort()
+            print(lst)
+            lst =''.join(lst)
+            if lst in ords:
+                ords[lst] += 1
+            else:
+                ords[lst] = 1
+        else:
+            for idx in range(len(order)):
+                if check[idx] == 0:
+                    check[idx] = 1
+                    lst.append(order[idx])
+                    DFS(lst,order, num)
+                    check[idx] = 0
+                    lst.pop()
+    for num in course:
+        ords = dict()
+        for order in orders:
+            if len(order) >= num:
+                check = [0 for i in range(len(order))]
+                order = list(order)
+                lst= []
+                DFS(lst, order, num)
+        print(ords)
     answer = []
-    od = dict()
-    for order in orders:
-
     return answer
+solution(orders,course)
