63e1dec1f71ae4ed1f0e7a0a6b2873dd17d602ed
unknown <wkfyddl1tp@naver.com>
2022-04-29 16:21:23 +0900

    modify

--- level_2/행렬테두리회전.py
+++ level_2/행렬테두리회전.py
@@ -7,7 +7,9 @@ def solution(rows, columns, queries):
     lst = [[i + (columns * j) for i in range(1, rows + 1)] for j in range(0, columns)]
     for query in queries:
         temp = 0
-        
+        temp = lst[query[0]query[1]]
+        for idx in range(query[1], query[3] + 1):
+            
     print(lst)
     return answer
 solution(rows, columns, queries)
