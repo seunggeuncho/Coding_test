1d827ec73b35f2d83c6f8f2b1eeef9b916f59587
unknown <wkfyddl1tp@naver.com>
2022-05-04 00:55:55 +0900

    final

--- level_2/행렬테두리회전.py
+++ level_2/행렬테두리회전.py
@@ -1,14 +1,27 @@
-rows = 6
-columns = 6
-queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
-
 def solution(rows, columns, queries):
     answer = []
-    lst = [[i + (columns * j) for i in range(1, rows + 1)] for j in range(0, columns)]
-    for query in queries:
-        temp = 0
-        temp = lst[query[0]query[1]]
-        for idx in range(query[1], query[3] + 1):
-            print(idx)
+    lst = [[i + (columns * j) for i in range(1, columns + 1)] for j in range(0, rows)]
+    print(lst)
+    for x1,y1,x2,y2 in queries:
+        temp = lst[x1 - 1][y1 - 1]
+        mini = temp
+        
+        for idx in range(x1 - 1, x2 - 1):
+            tmp = lst[idx + 1][y1 -1]
+            lst[idx][y1 - 1] = tmp
+            mini = min(tmp, mini)
+        for idx in range(y1 - 1, y2 - 1):
+            tmp = lst[x2 - 1][idx + 1]
+            lst[x2 - 1][idx] = tmp
+            mini = min(tmp, mini)
+        for idx in range(x2 - 1, x1 - 1, -1):
+            tmp = lst[idx - 1][y2 - 1]
+            lst[idx][y2 -1]=tmp
+            mini = min(tmp,mini)
+        for idx in range(y2 -1, y1 - 1, -1):
+            tmp = lst[x1 - 1][idx -1]
+            lst[x1 - 1][idx] = tmp
+            mini = min(mini, tmp)
+        lst[x1 - 1][y1] = temp
+        answer.append(mini)
     return answer
-solution(rows, columns, queries)
