b7dd1a8aacf42a99cb7ff8b367c5291191e91a95
seunggeuncho <wkfyddl1tp@naver.com>
2023-06-03 17:52:57 +0900

    not

---
+++ 백트래킹/수들의 합 4.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.stdin = open("test",'r')
+N,K = map(int, input().split())
+lst = list(map(int, input().split()))
+
+def check(idx):
+    total = 0
+    for v in range(lst):
+        total 
+
+for idx in range(1, N + 1):
+    check(idx)
