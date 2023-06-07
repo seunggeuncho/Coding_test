84275bc34b43d65ebef47b2601db39247fe0aa80
seunggeuncho <wkfyddl1tp@naver.com>
2023-06-07 16:03:15 +0900

    fail

--- 분할정복/색종이 만들기.py
+++ 분할정복/색종이 만들기.py
@@ -1,3 +1,36 @@
 import sys
 
-sys.stdin = open("test",'r')
+sys.stdin = open('test','r')
+
+N = int(input())
+lst = [list(map(int, input().split())) for _ in range(N)]
+zero = 0
+one = 0
+lst_len = len(lst)
+for r in range(lst_len):
+    for c in range(lst_len):
+        if lst[r][c] != -1:
+            cnt = 1
+            anchor = lst[r][c]
+            check = 1
+            while 1:
+                if cnt + r > N or cnt + c > N:
+                    break
+                for i in range(cnt):
+                    for val in lst[r + i][c:c+cnt]:
+                        if val != anchor:
+                            check = 0
+                            break
+                if check == 0:
+                    break
+                cnt += 1
+            for row in range(cnt - 1):
+                for col in range(cnt - 1):
+                    lst[r + row][c + col] = -1
+            if anchor == 0:
+                zero += 1
+            elif anchor == 1:
+                one += 1
+
+print(zero)
+print(one)
