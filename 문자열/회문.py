d40ba7869be9a3a7e7ed505667c10baac19a8b94
seunggeuncho <wkfyddl1tp@naver.com>
2023-06-10 14:44:05 +0900

    success

--- 문자열/회문.py
+++ 문자열/회문.py
@@ -2,7 +2,7 @@ import sys
 
 sys.stdin = open('test','r')
 T = int(input())
-
+"""
 for _ in range(T):
     str = list(input())
     if str == str[::-1]:
@@ -18,3 +18,32 @@ for _ in range(T):
     if check == 0:
         print(2)
 
+"""
+for _ in range(T):
+    str = list(input())
+    last = len(str) - 1
+    start = 0
+    check = 1
+    while start <= last:
+        if str[start] == str[last]:
+            start += 1
+            last -= 1
+            continue
+        if str[start] != str[last]:
+            tmp = str[:start] + str[start + 1:]
+            if tmp == tmp[::-1]:
+                check = 0
+                print(1)
+                break
+            tmp = str[:last] + str[last + 1:]
+            if tmp == tmp[::-1]:
+                print(1)
+                check = 0
+                break
+            break
+
+    if start >= last:
+        print(0)
+    else:
+        if check == 1:
+            print(2)
