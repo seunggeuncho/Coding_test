e16c280aefc2e4cc667ec5e3e95413cd65eeca5f
seunggeuncho <wkfyddl1tp@naver.com>
2023-06-28 14:04:17 +0900

    success

--- 완전탐색/큰 수 구성하기.py
+++ 완전탐색/큰 수 구성하기.py
@@ -1,13 +1,16 @@
 import sys
-sys.stdin = open("test.txt",'r')
+
 n, k = map(int, input().split())
 lst = list(map(str,input().split()))
 ans = 0
+
+
 def check(val):
-    if len(val) == len(str(n)):
+    if len(val) <= len(str(n)) and val:
         global ans
         if int(''.join(val)) <= n:
            ans = max(ans, int(''.join(val)))
+    if len(val) > len(str(n)):
         return
     for idx in lst:
         val.append(idx)
