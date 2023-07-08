c47763a364530ba74974bf87b042d18195bca74e
seunggeuncho <wkfyddl1tp@naver.com>
2023-07-08 17:25:41 +0900

    success

--- 분할정복/쿼드트리.py
+++ 분할정복/쿼드트리.py
@@ -5,6 +5,23 @@ n = int(input())
 lst = []
 for _ in range(n):
     lst.append(list(map(int, input())))
-print(lst)
+global ans
+ans = ''
+def check(x,y,n):
+    global ans
+    stand = lst[x][y]
+    for r in range(x, x + n):
+        for c in range(y, y + n):
+            if stand != lst[r][c]:
+                ans += '('
+                check(x, y, n // 2)
+                check(x, y + n // 2, n //2)
+                check(x + n // 2, y, n // 2)
+                check(x + n // 2, y + n // 2, n // 2)
+                ans += ')'
+                return
+    ans += str(stand)
 
-def check(n, ):
+
+check(0,0,n)
+print(ans)
