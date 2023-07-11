b1322a0b43652e8c3e11122d716c0766b1ad3c2a
seunggeuncho <wkfyddl1tp@naver.com>
2023-07-11 23:47:55 +0900

    fail

--- 누적합/나머지 합.py
+++ 누적합/나머지 합.py
@@ -1,19 +1,23 @@
 import sys
+from collections import defaultdict
 
 sys.stdin = open("test.txt",'r')
 
 n,m = map(int, input().split())
 lst = list(map(int,input().split()))
 lst_len = len(lst)
+dic = defaultdict(int)
 ans = 0
 for idx in range(1,lst_len):
     lst[idx] = lst[idx] + lst[idx -1]
 
-for f in range(lst_len):
-    if lst[f] % m == 0:
+for val in lst:
+    if val % m == 0:
         ans += 1
-    for n in range(f + 1,lst_len):
-        if (lst[n] - lst[f]) % m == 0:
-            ans += 1
+    tmp = 1
+    while val - tmp * m >= 0:
+        ans += dic[val - tmp * m]
+        tmp += 1
+    dic[val] += 1
 
 print(ans)
