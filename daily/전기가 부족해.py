368467a9da4de98dcdcdd4a6c97b87587515ca81
seunggeuncho <wkfyddl1tp@naver.com>
2024-02-06 17:27:32 +0900

    '240206'

--- daily/전기가 부족해.py
+++ daily/전기가 부족해.py
@@ -7,22 +7,34 @@ n, m, k = map(int, input().split())
 power = list(map(int, input().split()))
 
 graph = []
-check = [0] * (n + 1)
+parent = [i for i in range(n + 1)]
+
+for p in power:
+    parent[p] = 0
 
 for _ in range(m):
     u,v,w = map(int, input().split())
     graph.append([w,u,v])
-    graph.sort()
+graph.sort()
+
+def find(parent, node):
+    if parent[node] != node:
+        return find(parent, parent[node])
+    return node
+
+def union(parent,u,v):
+    parent_u = find(parent, u)
+    parent_v = find(parent, v)
+    if parent_u < parent_v:
+        parent[parent_v] = parent_u
+    else:
+        parent[parent_u] = parent_v
 
 answer = 0
 for w,u,v in graph:
-    if check[u] == 1 and check[v] == 1:
-        continue
-    else:
-        print(w,u,v)
-        check[u] = 1
-        check[v] = 1
+    if find(parent, u) != find(parent,v):
+        union(parent,u,v)
         answer += w
-        if sum(check) == n:
-            break
+    if sum(parent) == 0:
+        break
 print(answer)
