f68098d55343b0e6d015739b6293d792f1ddc565
seunggeuncho <wkfyddl1tp@naver.com>
2024-02-09 23:25:26 +0900

    '240209'

--- daily/크게 만들기.py
+++ daily/크게 만들기.py
@@ -3,6 +3,18 @@ import sys
 sys.stdin = open('test.txt','r')
 
 n,k = map(int, input().split())
-num = int(input())
+final = n - k
+num = list(input())
+answer = []
 
-
+while len(answer) < final:
+    strd = num[0]
+    indx = 0
+    for idx in range(1,1 + k):
+        if strd < num[idx]:
+            strd = num[idx]
+            indx = idx
+    answer.append(strd)
+    num = num[indx + 1:]
+    k -= indx
+print(int(''.join(answer)))
