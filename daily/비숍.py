8ca8d94a9572f7017a59e25c93d269e1d8b14cbb
seunggeuncho <wkfyddl1tp@naver.com>
2024-02-15 00:32:17 +0900

    240215

--- daily/비숍.py
+++ daily/비숍.py
@@ -6,3 +6,24 @@ size = int(input())
 
 chess = [list(map(int, input().split())) for _ in range(size)]
 
+answer = 0
+
+
+for row in range(size):
+    for col in range(size):
+        if chess[row][col] == 1:
+            flag = 0
+            for i in range(1,size):
+                l_row, l_col = row - i, col - i
+                r_row, r_col = row - i, col + i
+                if 0 <= l_row < size and 0<= l_col < size:
+                    if chess[l_row][l_col] == 2:
+                        flag = 1
+                if 0 <= r_row < size and 0 <= r_col < size:
+                    if chess[r_row][r_col] == 2:
+                        flag = 1
+            if flag == 0:
+                answer+= 1
+                chess[row][col] = 2
+print(chess)
+print(answer)
