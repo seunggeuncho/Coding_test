f97402909cb0a85cab8c24aabbdb1324e08b07dc
seunggeuncho <wkfyddl1tp@naver.com>
2024-04-07 20:21:25 +0900

    240407

--- daily/미로탈출.py
+++ daily/미로탈출.py
@@ -1,6 +1,6 @@
 from collections import deque
 import sys
-import copy
+
 
 sys.stdin = open('test.txt','r')
 
@@ -18,36 +18,26 @@ lst.append([0 for _ in range(m + 2)])
 
 ans = float('inf')
 def bfs(lst):
-    check = [[0 for _ in range(m + 2)] for _ in range(n + 2)]
-    q = deque([[h1, h2,0]])
-    check[h1][h2] = 1
+    check = [[[False for _ in range(2)] for _ in range(m + 2)] for _ in range(n + 2)]
+    q = deque([[h1, h2,0,1]])
+    check[h1][h2][1] = 1
     while q:
-        x,y,val = q.popleft()
+        print(q)
+        x,y,val,flag = q.popleft()
         if x == e1 and y == e2:
             return val
         for idx in range(4):
             changeX = x + dir[idx][0]
             changeY = y + dir[idx][1]
             if 1<= changeX <= n and 1<=changeY <= m:
-                if lst[changeX][changeY] == 0 and check[changeX][changeY] == 0:
-                    check[changeX][changeY] = 1
-                    q.append([changeX, changeY, val + 1])
-
+                if check[changeX][changeY][flag]:
+                    continue
+                elif lst[changeX][changeY] == 0:
+                    check[changeX][changeY][flag] = 1
+                    q.append([changeX, changeY, val + 1,flag])
+                elif lst[changeX][changeY] == 1 and flag == 1:
+                    check[changeX][changeY][0] = 1
+                    q.append([changeX, changeY, val + 1, 0])
     return -1
-result = bfs(lst)
-if result != -1:
-    ans = min(ans, result)
+print(bfs(lst))
 
-for r in range(n):
-    for c in range(m):
-        if lst[r][c] == 1:
-            result = 0
-            t_lst = copy.deepcopy(lst)
-            t_lst[r][c] = 0
-            result = bfs(t_lst)
-            if result != -1:
-                ans = min(ans, result)
-if ans == float('inf'):
-    print(-1)
-else:
-    print(ans)
