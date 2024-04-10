aeed28ceda68f2493de435ec58bfebdcbf739a7e
seunggeuncho <wkfyddl1tp@naver.com>
2024-04-10 23:51:55 +0900

    240410

---
+++ daily/두 배열의 합.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.stdin = open('test.txt','r')
+
+t = int(input())
+n = int(input())
+lst1 = list(map(int, input().split()))
+m = int(input())
+lst2 = list(map(int, input().split()))
+
