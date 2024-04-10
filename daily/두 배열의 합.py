9aa9303c8048f53e047dad8f4f87dabf688bc958
seunggeuncho <wkfyddl1tp@naver.com>
2024-04-11 00:37:16 +0900

    240411

--- daily/두 배열의 합.py
+++ daily/두 배열의 합.py
@@ -1,4 +1,5 @@
 import sys
+import bisect
 
 sys.stdin = open('test.txt','r')
 
@@ -8,3 +9,25 @@ lst1 = list(map(int, input().split()))
 m = int(input())
 lst2 = list(map(int, input().split()))
 
+result = 0
+sum1 = lst1
+sum2 = lst2
+
+for start in range(n):
+    for end in range(start + 1, n):
+        sum1.append(sum(lst1[start:end + 1]))
+
+
+for start in range(m):
+    for end in range(start + 1, m):
+        sum2.append(sum(lst2[start:end + 1]))
+
+
+sum1.sort()
+sum2.sort()
+
+for i in range(len(sum1)):
+    left = bisect.bisect_left(sum2, t - sum1[i])
+    right = bisect.bisect_right(sum2, t - sum1[i])
+    result += right - left
+print(result)
