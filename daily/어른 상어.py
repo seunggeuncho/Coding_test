afbb73af1c3a5f5a718ca9ff59d7a214f8894cd0
seunggeuncho <wkfyddl1tp@naver.com>
2024-04-15 23:45:48 +0900

    240415

--- daily/어른 상어.py
+++ daily/어른 상어.py
@@ -2,3 +2,17 @@ import sys
 
 sys.stdin = open('test.txt','r')
 
+n,m,k = map(int, input().split())
+
+lst = []
+
+for _ in range(n):
+    lst.append(list(map(int, input().split())))
+
+now_dir = list(map(int, input().split()))
+shark_dir = [[] for _ in range(m)]
+
+for idx in range(m):
+    for _ in range(4):
+        shark_dir[idx].append(list(map(int, input().split())))
+
